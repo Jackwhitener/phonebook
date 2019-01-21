84967dbb2b9b2d1642ed2e944d9be36c1f446edf
Jackwhitener <jackwhitener1@gmail.com>
2019-01-21 13:16:17 -0500

    I rearranged it to be better, and added a hard-coded login

--- app.rb
+++ app.rb
@@ -1,20 +1,24 @@
 require 'sinatra'
 require 'csv'
 get '/' do
-	erb :home
+    erb :home
 end
+post '/loginlander' do
+    user = params[:username]
+    pass = params[:password]
+    redirect '/loginlander?user=' + user.to_s + '&pass=' + pass.to_s
 
+end
+get '/loginlander' do
+    user = params[:user]
+    pass = params[:pass]
+    erb :loginlander, locals: {user: user, pass: pass}
+end
 post '/list' do
     redirect '/list?'
 end
 get '/list' do
-    erb :list
-end
-post '/addperson' do
-    redirect '/list?'
-end
-get '/addperson' do
-    erb :addperson
+erb :list
 end
 post '/register' do
     redirect '/register?'
@@ -24,4 +28,16 @@ get '/register' do
 end
 post '/registered' do
     redirect '/list?'
+end
+post '/viewcon' do
+    redirect '/viewcon?'
+end
+get '/viewcon' do
+    erb :view
+end
+post '/editcon' do
+    redirect '/edit?'
+end
+get '/editcon' do
+    erb :edit
 end
