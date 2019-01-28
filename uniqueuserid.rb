bd75bac331909a0c2a7a0e680e7ebd97aa99322e
Jackwhitener <jackwhitener1@gmail.com>
2019-01-28 15:59:56 -0500

    Next test, checks to make sure it returns an integer.

--- uniqueuserid.rb
+++ uniqueuserid.rb
@@ -1,3 +1,3 @@
-def userid
+def uuid
 
 end
--- uniqueuserid_test.rb
+++ uniqueuserid_test.rb
@@ -1,6 +1,11 @@
 require "minitest/autorun"
+require_relative 'uniqueuserid.rb'
 class My_test < Minitest::Test
     def test_1
         assert_equal(1,1)
+    end
+    def test_for_class
+        userid = uuid()
+        assert_equal(Integer, userid.class)
     end
 end
