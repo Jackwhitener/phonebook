96f1613f229f92df83972541c518f005bad8c45e
Jackwhitener <jackwhitener1@gmail.com>
2019-01-30 09:52:16 -0500

    turns out uuid is a sequel method, and other changes

--- sql.rb
+++ sql.rb
@@ -2,7 +2,28 @@ require 'mysql2'
 require_relative 'uniqueuserid.rb'
 require_relative 'bcrypt.rb'
 load "./local_ENV.rb" if File.exist?('./local_ENV.rb')
+def client
+    Mysql2::Client.new(
+      :host => ENV['host'],
+      :username => ENV['username'],
+      :password => ENV['password'],
+      :database => ENV['database'],
+      :port => ENV['port']
+)
+end
+def check_if_taken(username)
+    check = client.query(
+        "SELECT username FROM users WHERE users.username = '#{username}'"
+        )
+        return check.count > 0
+end
 def add_user(username, password)
     password = hashpass(password)
-    userid = userid()
+    if !check_if_taken(username)
+        client.query(
+          "INSERT INTO users (userid, username, password)
+          VALUES (UUID(), '#{username}', '#{password}')"
+        )
+    end
 end
+add_user('admin', 'password')
--- uniqueuserid.rb
+++ uniqueuserid.rb
@@ -1,3 +1,8 @@
 def uuid
-    1111111111111111
+    arr = ['0','1','2','3','4','5','6','7','8','9']
+    id = String.new
+    16.times do
+        id += arr.sample
+    end
+    return id.to_i
 end
