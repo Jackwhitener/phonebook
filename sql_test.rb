24cf5a38adf2caf91c637c972bb4cc66e9f42fc1
Jackwhitener <jackwhitener1@gmail.com>
2019-02-04 09:08:43 -0500

    adds a test, that checks for an integer, instead of a string

--- sql_test.rb
+++ sql_test.rb
@@ -10,4 +10,7 @@ class My_test < Minitest::Test
     def test_check_if_not_taken
         assert_equal(false, check_if_taken('smittywerberjaegermanjensen'))
     end
+    def test_check_if_not_taken_non_string
+        assert_equal(false,check_if_taken(123))
+    end
 end
