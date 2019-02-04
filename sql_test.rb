104d8ef0e1e98d3061d673b52d14d90da53f1f60
Jackwhitener <jackwhitener1@gmail.com>
2019-02-04 09:03:43 -0500

    adds test for check_if_taken, passes

--- sql_test.rb
+++ sql_test.rb
@@ -4,4 +4,8 @@ class My_test < Minitest::Test
     def test_1
         assert_equal(1,1)
     end
+    def test_check_if_taken
+        assert_equal(true, check_if_taken('admin'))
+    end
+    
 end
